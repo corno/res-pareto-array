bcf73f688cfe5829ba860a2a197bed712e4ad8f7
corno <corno.astn.a@schraverus.com>
2023-02-23 21:14:42 +0100

    .

--- pareto/src/bin/generateCode.generated.ts
+++ pareto/src/bin/generateCode.generated.ts
@@ -1,11 +1,11 @@
 import * as pe from 'pareto-core-exe'
 
-import * as gpareto from "lib-pareto-typescript-project"
+import * as mpareto from "lib-pareto-typescript-project"
 
 import { $ as project } from "../data/project.data"
 
 pe.runProgram(($) => {
-    gpareto.$a.generateProject({
+    mpareto.$a.generateProject({
         project: project,
         mainData: $,
     })
