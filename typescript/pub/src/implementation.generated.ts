7553404d4a5c098e131d5f6c30124d88cadc0408
corno <corno.astn.a@schraverus.com>
2023-04-11 15:37:56 +0200

    .

--- typescript/pub/src/implementation.generated.ts
+++ typescript/pub/src/implementation.generated.ts
@@ -2,7 +2,7 @@ import { API } from "./api.generated"
 import { $$ as igetElementAt } from "./implementations/getElementAt.native"
 import { $$ as igetLength } from "./implementations/getLength.native"
 
-export const $r: API = {
+export const $api: API = {
     'getElementAt': igetElementAt,
     'getLength': igetLength,
 }
